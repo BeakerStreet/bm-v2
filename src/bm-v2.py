3e257d16fd7211c336decb9bef6fabc93bd6eca1
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-01-31 08:24:15 +0000

    adds matplot and pandas

--- src/bm-v2.py
+++ src/bm-v2.py
@@ -1,4 +1,5 @@
 
+
 class bmv2:
     def __init__(self):
         self.model = pass
--- utils/setup.py
+++ utils/setup.py
@@ -4,8 +4,8 @@ import boto3
 
 def main():
     s3 = boto3.client('s3')
-    
-    # download old jsons from bm-v1
+
+   # jupyter eval: "are there turns 1-10 actions and game_states by player"
     # reformat into new json
     # save locally, upload to s3
     # write data cleaning scripts
