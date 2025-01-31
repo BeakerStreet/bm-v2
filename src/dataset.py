146fe8f53ec580699f33ab4ae6b484a222fea1da
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-01-31 08:10:04 +0000

    adds bm-v2.py and dataset.py plans

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,13 @@
 
 
 class Dataset:
-    def __init__(self, ):
-        # 
-        
+    def __init__(self, bucket: str):
+        self.bucket = bucket
+        self.raw = pass
+        self.cleaned = pass
+        self.embeddings = pass
     
+    '''
+    processing text datasets reflecting game_states
+    and player actions. Intent is an unsupervised 
+    learning algorithm 
