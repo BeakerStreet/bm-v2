9be05ef5000bdc0a97b8ed98ec5fab8efed4ee34
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-02-02 14:54:45 +0000

    messes about with Folders a bit

--- utils/setup.py
+++ utils/setup.py
@@ -1,8 +1,8 @@
 import boto3
 import os
-import boto3.session as s3
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -12,35 +12,62 @@ SCOPES = os.getenv('SCOPES').strip("[]").replace("'", "").split(", ")
 
 def authenticate_gdrive():
     
-    # Authenticate with service account
-    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
     # Build the Drive API client
-    service = build('drive', 'v3', credentials=credentials)
+    service = gdrive_build()
 
     # Get the name of the folder
-    folder_id = os.getenv('FOLDER_ID')
+    folder_id = os.getenv('GDRIVE_ROOT_FOLDER_ID')
     folder = service.files().get(fileId=folder_id, fields='name').execute()
     folder_name = folder.get('name')
 
     # Return a success message
-    response = f"Successfully accessed folder: {folder_name}"
+    response = f"{folder_name}/"
 
     return response
 
-def download_file_from_gdrive(drive, file_id, dest_path):
-    file = drive.CreateFile({'id': file_id})
-    file.GetContentFile(dest_path)
+def gdrive_build():
+    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    service = build('drive', 'v3', credentials=credentials)
 
+    return service
 
+def gdrive_download_file(service, path, file):
+    
+    # Extract the file ID from the path
+    file_id = path.split('/')[-1]
+    
+    # Request the file from Google Drive
+    request = service.files().get_media(fileId=file_id)
+    
+    with open(file, 'wb') as f:
+        downloader = MediaIoBaseDownload(f, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
+    
+    return file
+
+def gdrive_get_folder_list(service, path):
+    files = []
+    folder_id = path.split('/')[-1]
+    
+    # List folders
+    results = service.files().list(
+        q="mimeType='application/vnd.google-apps.folder'",
+        fields="nextPageToken, files(id, name)"
+    ).execute()
+    folders = results.get('files', [])
+
+    return folders
 
 def main():
     s3 = boto3.client('s3')
 
-    auth_response = authenticate_gdrive()
-    print(auth_response)
+    root_folder = authenticate_gdrive()
+    service = gdrive_build()
+    folders = gdrive_get_folder_list(service, f"{root_folder}/training_data")
     
-    # sort access
+
     # folder dict: {'name': name, 'id': id}, 
     # for name, id in dict: 
     #   for image_filename in id:
