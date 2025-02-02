134f2f89c2ae106793aa9af730e3c3880a3b6b86
Jonathan Holtby <holtby.jonathan@gmail.com>
2025-02-02 12:52:58 +0000

    adds plan!

--- utils/setup.py
+++ utils/setup.py
@@ -1,10 +1,48 @@
 import boto3
 import os
 import boto3.session as s3
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+def authenticate_gdrive():
+    # to do: init gdrive auth
+    return drive
+
+def download_file_from_gdrive(drive, file_id, dest_path):
+    file = drive.CreateFile({'id': file_id})
+    file.GetContentFile(dest_path)
+
+
 
 def main():
     s3 = boto3.client('s3')
 
+    drive = authenticate_gdrive()
+    print("auth successful")
     
+    # sort access
+    # folder dict: {'name': name, 'id': id}, 
+    # for name, id in dict: 
+    #   for image_filename in id:
+    #       print(image_filename)
+    #       input() just take a look
+
+    '''
+    Plan:
+
+    - create the labeling sequence in dataset:
+        > video -> images (done)
+        > images -> build orders
+        > build orders -> dataset.json
+        > (opt) plot build orders 
+    - create the mod prediction sequence in bmv2:
+        > build orders -> PseudoYields
+        > PseudoYields -> mod code
+    - ai-generate some model tutorials: 
+        > a multi-modal input that predicts the build order data in one shot
+        > a transformer taking the images, generating a bunch of text, and outputting the mod code directly
+    '''
+
+
 if __name__ == "__main__":
     main()
